e7a1a32875be95898ef921d77149d5020a62d0a4
marcustate <58437889+marcustate@users.noreply.github.com>
2020-03-11 23:21:01 -0500

    final commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 const express = require("express");
 const path = require("path");
-const notes = require ("./db/db.json");
+const notes = require("./db/db.json");
 
 
 const app = express();
@@ -12,40 +12,40 @@ const PORT = 5500;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/",(req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 })
 
-app.get("/notes",(req, res) => {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
 
-app.get("/assets/js/",(req, res) => {
+app.get("/assets/js/", (req, res) => {
 
-        res.sendFile(path.join(__dirname, "./public/assets/js/index.js"));
+    res.sendFile(path.join(__dirname, "./public/assets/js/index.js"));
 })
 
-app.get("/assets/css",(req, res) => {
+app.get("/assets/css", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/assets/css/styles.css"));
 })
 
-app.get("/api/notes/",(req, res) => {
+app.get("/api/notes/", (req, res) => {
     return res.json(notes);
 })
 
-app.post("/api/notes/",(req, res) => {
+app.post("/api/notes/", (req, res) => {
     console.log(req.body);
     req.body.id = notes.length + 1;
     notes.push(req.body);
     return res.json(notes);
-    })
+})
 
-app.delete("/api/notes/:id",(req, res) => {
+app.delete("/api/notes/:id", (req, res) => {
     console.log()
-    
+    res.send("/api/notes" + id);
     return res.json(notes);
 })
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
