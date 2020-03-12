b7d58499b8f8818ed2ce76a229eaa05723bc2dee
marcustate <58437889+marcustate@users.noreply.github.com>
2020-03-12 17:32:57 -0500

    adding procfile

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const notes = require("./db/db.json");
 
 
 const app = express();
-const PORT = 5500;
+const PORT = process.env.PORT || 5500;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
