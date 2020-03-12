1b3aded66a598cd4fea03c4b73f0a1bd68a7942a
marcustate <58437889+marcustate@users.noreply.github.com>
2020-03-11 22:43:19 -0500

    pushing polished code to master

---
+++ server.js
@@ -0,0 +1,51 @@
+// Dependencies 
+// ========================================
+
+const express = require("express");
+const path = require("path");
+const notes = require ("./db/db.json");
+
+
+const app = express();
+const PORT = 5500;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/",(req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+app.get("/notes",(req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
+app.get("/assets/js/",(req, res) => {
+
+        res.sendFile(path.join(__dirname, "./public/assets/js/index.js"));
+})
+
+app.get("/assets/css",(req, res) => {
+    res.sendFile(path.join(__dirname, "./public/assets/css/styles.css"));
+})
+
+app.get("/api/notes/",(req, res) => {
+    return res.json(notes);
+})
+
+app.post("/api/notes/",(req, res) => {
+    console.log(req.body);
+    req.body.id = notes.length + 1;
+    notes.push(req.body);
+    return res.json(notes);
+    })
+
+app.delete("/api/notes/:id",(req, res) => {
+    console.log()
+    
+    return res.json(notes);
+})
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
